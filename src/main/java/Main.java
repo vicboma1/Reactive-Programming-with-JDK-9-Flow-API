a5dbaf684652a22848cbadae9de0b821ef6680b9
Victor Bolinches <vicboma1@users.noreply.github.com>
2017-09-19 20:50:14 +0200

    EntryPoint Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
 
-    public  static  void main (String args []){
+    public static void main (String args []) throws InvocationTargetException, InterruptedException {
+        SwingUtilities.invokeAndWait(()->{
+            System.out.print("Hello Reactive World!");
+        });
 
     }
 }
