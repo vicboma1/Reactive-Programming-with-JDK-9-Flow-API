e0f86151b268f4507e4fc116e0a5d163e7bf4fde
Victor Bolinches <vicboma1@users.noreply.github.com>
2017-09-19 22:29:00 +0200

    Main example - Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,24 @@ public class Main {
     public static void main (String args []) throws InvocationTargetException, InterruptedException {
         SwingUtilities.invokeAndWait(()->{
             System.out.print("Hello Reactive World!");
+
+            final Publisher1 publisher = Publisher1.create();
+
+            final Subscriber1 subscriber = Subscriber1.create();
+            subscriber.setDEMAND(4);
+
+            final Processor1 processor = Processor1.create();
+            processor.setDEMAND(5);
+
+            publisher.subscribe(processor);
+            processor.subscribe(subscriber);
+
+            try {
+                publisher.waitUntilTerminated();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         });
 
     }
