8de7aee8a5d992ec634542c2c7486cf20472e9a9
Victor Bolinches <vicboma1@users.noreply.github.com>
2017-09-19 18:06:59 +0200

    project configure java 9

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() throws  Exception{
+        Assert.assertTrue(true);
+    }
+}
